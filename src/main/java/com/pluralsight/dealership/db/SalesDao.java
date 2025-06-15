04bce2be0721804258e3a5d2ab9aefc8d61f04ad
Oumou09 <oumoudesigns@gmail.com>
2025-06-15 00:30:31 -0400

    finished the addSalesContract class and I used the same formula that I used for the lease contract for this one since it was asking for the same thing

--- src/main/java/com/pluralsight/dealership/db/SalesDao.java
+++ src/main/java/com/pluralsight/dealership/db/SalesDao.java
@@ -5,6 +5,7 @@ import com.pluralsight.dealership.models.SalesContract;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SalesDao {
@@ -16,5 +17,28 @@ public class SalesDao {
 
     public void addSalesContract(SalesContract salesContract) {
         // TODO: Implement the logic to add a sales contract
+        try(Connection connection = dataSource.getConnection();
+        PreparedStatement saleStatement = connection.prepareStatement("""
+                INSERT INTO sales_contracts (VIN, sale_date, price)
+                VALUES (?, ?, ?)""",
+                PreparedStatement.RETURN_GENERATED_KEYS)){
+            saleStatement.setString(1, salesContract.getVin());
+            saleStatement.setDate(2, java.sql.Date.valueOf(salesContract.getSaleDate()));
+            saleStatement.setDouble(3, salesContract.getPrice());
+            saleStatement.executeUpdate();
+
+            try (ResultSet generatedKeys = saleStatement.getGeneratedKeys()) {
+                if(generatedKeys.next()){
+                    int contractID = generatedKeys.getInt(1);
+                    System.out.println("New Contract ID: " + salesContract.getContractId());
+
+                }
+
+            }
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
